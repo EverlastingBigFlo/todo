eb1bfcc76936307512309ca660fce421a02ce7a9
EverlastingLilflo <anjorin199@gmail.com>
2023-11-20 12:33:02 +0100

    tried animated text to my code

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/components/clock.dart';
 import 'package:todo/components/my_text.dart';
@@ -100,6 +101,18 @@ class _HomePageState extends State<HomePage> {
                   Row(
                     children: [
                       //user welcome
+                      // DefaultTextStyle(
+                      //   style: const TextStyle(
+                      //     fontSize: 20.0,
+                      //   ),
+                      //   child: AnimatedTextKit(
+                      //     animatedTexts: [
+                      //       WavyAnimatedText('Hello ${getGreeting()}'),
+                      //     ],
+                      //     isRepeatingAnimation: true,
+                      //     onTap: () {},
+                      //   ),
+                      // ),
                       MyText(
                           text: getGreeting(),
                           fontSize: 18,
