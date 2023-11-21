1ff6a548d99140ebb7414553883bbaa22578ab94
EverlastingLilflo <anjorin199@gmail.com>
2023-11-21 21:16:25 +0100

    added text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,118 +30,114 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: const Color(0xFFF0F4F3),
-      body: Container(
-        child: Column(
-          children: [
-            Stack(
-              children: <Widget>[
-                Positioned(
-                  child: Container(
-                    width: 430,
-                    height: 292,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF50C2C9),
-                      // borderRadius: BorderRadius.circular(100.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 5,
-                          blurRadius: 20,
-                        ),
-                      ],
-                    ),
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              Positioned(
+                child: Container(
+                  width: 430,
+                  height: 292,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF50C2C9),
+                    // borderRadius: BorderRadius.circular(100.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 20,
+                      ),
+                    ],
                   ),
                 ),
-                Positioned(
-                  left: -100,
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: myColor.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(100.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 5,
-                          blurRadius: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: -90,
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: myColor.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(100.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-                // adding image to my background
-
-                Positioned(
-                  bottom: 5,
-                  left: 140,
-                  child: Center(
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Clock(),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            //user welcome
-                            // DefaultTextStyle(
-                            //   style: const TextStyle(
-                            //     fontSize: 20.0,
-                            //   ),
-                            //   child: AnimatedTextKit(
-                            //     animatedTexts: [
-                            //       WavyAnimatedText('Hello ${getGreeting()}'),
-                            //     ],
-                            //     isRepeatingAnimation: true,
-                            //     onTap: () {},
-                            //   ),
-                            // ),
-                            MyText(
-                                text: getGreeting(),
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              child: Column(
-                children: [
-                  MyText(
-                      text: 'text',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)
-                ],
               ),
-            )
-          ],
-        ),
+              Positioned(
+                left: -100,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: myColor.withOpacity(0.7),
+                    borderRadius: BorderRadius.circular(100.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: -90,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: myColor.withOpacity(0.7),
+                    borderRadius: BorderRadius.circular(100.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // adding image to my background
+
+              Positioned(
+                bottom: 5,
+                left: 140,
+                child: Center(
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Clock(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          //user welcome
+                          // DefaultTextStyle(
+                          //   style: const TextStyle(
+                          //     fontSize: 20.0,
+                          //   ),
+                          //   child: AnimatedTextKit(
+                          //     animatedTexts: [
+                          //       WavyAnimatedText('Hello ${getGreeting()}'),
+                          //     ],
+                          //     isRepeatingAnimation: true,
+                          //     onTap: () {},
+                          //   ),
+                          // ),
+                          MyText(
+                              text: getGreeting(),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              MyText(
+                  text: 'text',
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black)
+            ],
+          )
+        ],
       ),
     );
   }
