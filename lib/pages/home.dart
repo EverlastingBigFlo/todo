45bf3ee670c9db1f699d36aaac60d05578451c6c
EverlastingLilflo <anjorin199@gmail.com>
2023-12-29 22:09:18 +0100

    changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_super_parameters
 // import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/components/clock.dart';
